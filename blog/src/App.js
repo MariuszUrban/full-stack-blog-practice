0f0b3a6face5c7899b52d39b589aae382f30646d
Mariusz <urban.mariusz@icloud.com>
2020-12-29 20:25:13 +0100

    setup done

---
+++ blog/src/App.js
@@ -0,0 +1,25 @@
+import React  from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import ArticlesList from "./components/ArticlesList";
+import ArticlePage from "./components/ArticlePage";
+
+import "./App.css";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article-page" component={ArticlePage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
